1df0dd77f75801d15a92260e970a03774558b00c
Divij Jain <68986989+diiviij@users.noreply.github.com>
2022-11-09 20:02:58 +0530

    Opens three web pages

---
+++ Pay-load/basic2.bat
@@ -0,0 +1,5 @@
+@echo off
+start "webpage name" "http://github.com/"
+start "webpage name" "http://google.com/"
+start "webpage name" "http://diiviij.github.io/"
+start "webpage name" "http://instagram.com/"
